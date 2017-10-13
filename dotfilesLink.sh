70ba25125fe581de7f0a7655eba5644c02d55aad
kakiuchi <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2017-10-13 16:10:39 +0900

    add .zshrc .zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,5 @@ ln -sf ~/.dotfiles/.vimrc ~/.vimrc
 ln -sf ~/.dotfiles/colors ~/.vim
 ln -sf ~/.dotfiles/rc ~/.vim
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zsh ~/.zsh
