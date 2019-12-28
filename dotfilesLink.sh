151bdf046b9cfb2925d216c6a932d15163a68ec9
kacky24 <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2019-12-29 05:53:37 +0900

    config for prezto

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -sf ~/.dotfiles/.vim ~/.vim
 ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.zsh ~/.zsh
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/.zpreztorc ~/.zpreztorc
