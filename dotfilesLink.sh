6068c5340fd667701626fbcd70f8d73f8e9434c3
kacky24 <kakiuchi@crimson.q.t.u-tokyo.ac.jp>
2019-12-31 14:35:20 +0900

    update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,4 @@ ln -sf ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/.zsh ~/.zsh
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/.zpreztorc ~/.zpreztorc
-mkdir ~/.config/nvim
-ln -sf ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+ln -sf ~/.dotfiles/nvim ~/.config/nvim
